2678ade5c56217b622c140401da6609e5d90f35c
renatho <renatho.20gomes@gmail.com>
2024-02-21 10:33:49 -0300

    corrigindo migration para ts

---
+++ app/backend/migrations/20240220192007-01-teams.ts
@@ -0,0 +1,19 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import { Team } from '../src/types/teams';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<Team>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+};
