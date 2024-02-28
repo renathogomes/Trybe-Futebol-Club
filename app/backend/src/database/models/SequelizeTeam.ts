eca6acb05e6328acd1c37435fc5d2089eb869dba
renatho <renatho.20gomes@gmail.com>
2024-02-28 10:34:02 -0300

    seguindo o passo a passo do Trybeteca

---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,38 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreateOptions,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<
+InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>
+> {
+  declare id: CreateOptions<number>;
+  declare teamName: string;
+}
+
+SequelizeTeam.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'team',
+    timestamps: false,
+  },
+);
+
+export default SequelizeTeam;
