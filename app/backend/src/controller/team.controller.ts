3e236f00a0cb52170bc715e54dbcc824e22ca95f
renatho <renatho.20gomes@gmail.com>
2024-02-28 13:28:53 -0300

    controller

---
+++ app/backend/src/controller/team.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import TeamService from '../services/team.service';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
+
+export default class TeamController {
+  constructor(
+    private teamService: TeamService = new TeamService(),
+  ) { }
+
+  public async getAllTeams(_req: Request, res: Response) {
+    const ServiceResponse = await this.teamService.getAllTeams();
+    res.status(mapStatusHTTP(ServiceResponse.status)).json(ServiceResponse.data);
+  }
+}
