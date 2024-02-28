dd20d3e9bcb509a3dc0d611d3105af48825e4ebd
renatho <renatho.20gomes@gmail.com>
2024-02-28 13:35:02 -0300

    router

---
+++ app/backend/src/routes/team.routes.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from 'express';
+import TeamController from '../controller/team.controller';
+
+const teamController = new TeamController();
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => teamController.getAllTeams(req, res));
+
+export default router;
