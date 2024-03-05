be1c72fc00596f435ad73b1b5236db96bab996eb
renatho <renatho.20gomes@gmail.com>
2024-03-05 15:57:52 -0300

    model user

---
+++ app/backend/src/model/user.model.ts
@@ -0,0 +1,13 @@
+import { IUser } from '../Interfaces/users/IUser';
+import { IUserModel } from '../Interfaces/users/IUserModel';
+import SequelizeUser from '../database/models/SequelizeUser';
+
+export default class UserModel implements IUserModel {
+  private model = SequelizeUser;
+
+  async findByEmail(email: string): Promise<IUser | null> {
+    const user = await this.model.findOne({ where: { email } });
+    if (!user) return null;
+    return user;
+  }
+}
