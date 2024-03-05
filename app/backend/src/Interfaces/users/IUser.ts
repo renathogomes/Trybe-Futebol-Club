c6a6500b688f993de02ecc1636db6816fb66edfa
renatho <renatho.20gomes@gmail.com>
2024-03-05 15:34:56 -0300

    migration user

--- app/backend/src/Interfaces/users/IUser.ts
+++ app/backend/src/Interfaces/users/IUser.ts
@@ -1,6 +1,7 @@
 import { Entity, EntityLogin } from '../index';
 
 export interface IUser extends Entity, EntityLogin {
+  name: string;
   password: string;
   role: string;
 }
---
+++ app/backend/src/database/migrations/02-create-users.ts
@@ -0,0 +1,36 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import { IUser } from '../../Interfaces/users/IUser';
+
+export default {
+    up(queryInterface: QueryInterface) {
+        return queryInterface.createTable<Model<IUser>>('users', {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            role: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        });
+    },
+    down(queryInterface: QueryInterface) {
+        return queryInterface.dropTable('users');
+    },
+};
