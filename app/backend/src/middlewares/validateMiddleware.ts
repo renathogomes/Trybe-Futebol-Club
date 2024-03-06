d43386c83a10843c219e31bbd03b9bf683fc078c
renatho <renatho.20gomes@gmail.com>
2024-03-06 17:40:30 -0300

    refazendo validacao token

--- app/backend/src/middlewares/validateMiddleware.ts
+++ app/backend/src/middlewares/validateMiddleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
 
 export default class ValidateMiddleware {
   static validateBody(req: Request, res: Response, next: NextFunction) {
@@ -14,9 +15,17 @@ export default class ValidateMiddleware {
   }
 
   static validateToken(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization;
-    if (!token) {
+    const token1 = req.headers.authorization;
+    if (!token1) {
       return res.status(401).json({ message: 'Token not found' });
+    }
+    const token = token1.split(' ')[1];
+    try {
+      const secret = process.env.SECRET ?? 'secret';
+      const decoded = jwt.verify(token, secret);
+      res.locals.authorization = decoded;
+    } catch (error) {
+      return res.status(401).json({ message: 'Invalid token' });
     }
     next();
   }
