87d6e6a64a2ee46721eb4ea887c2cdcc56480062
renatho <renatho.20gomes@gmail.com>
2024-03-08 14:46:39 -0300

    iniciando o leaderBoard

---
+++ app/backend/src/Interfaces/leaderBoard/ILeaderBord.ts
@@ -0,0 +1,11 @@
+export interface ILeaderBoard {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+}
