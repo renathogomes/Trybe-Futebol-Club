01919d5737b36adaac75c22308f4ebfda812511e
renatho <renatho.20gomes@gmail.com>
2024-03-08 13:38:01 -0300

    validacoes no matches

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -1,15 +1,28 @@
 import { Router, Request, Response } from 'express';
 import ControllerMatch from '../controller/match.controller';
+import Validate from '../middlewares/validateMiddleware';
 
 const router = Router();
 const matchController = new ControllerMatch();
 
 router.get('/', (req: Request, res: Response) => matchController.getAllMatches(req, res));
 
-router.post('/', (req: Request, res: Response) => matchController.createMatch(req, res));
+router.post(
+  '/',
+  Validate.validateToken,
+  (req: Request, res: Response) => matchController.createMatch(req, res),
+);
 
-router.patch('/:id', (req: Request, res: Response) => matchController.updateMatch(req, res));
+router.patch(
+  '/:id',
+  Validate.validateToken,
+  (req: Request, res: Response) => matchController.updateMatch(req, res),
+);
 
-router.patch('/:id/finish', (req: Request, res: Response) => matchController.endMatch(req, res));
+router.patch(
+  '/:id/finish',
+  Validate.validateToken,
+  (req: Request, res: Response) => matchController.endMatch(req, res),
+);
 
 export default router;
