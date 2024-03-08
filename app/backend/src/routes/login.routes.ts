0b6bf2e51bfb4414542e0882d16fec522027a8fb
renatho <renatho.20gomes@gmail.com>
2024-03-08 09:37:16 -0300

    alteração na rota

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import UserController from '../controller/user.controller';
 import ValidateLogin from '../middlewares/validateMiddleware';
@@ -7,8 +7,16 @@ const userController = new UserController();
 
 const router = Router();
 
-router.post('/', ValidateLogin.validateBody, (req, res) => userController.login(req, res));
+router.post(
+  '/',
+  ValidateLogin.validateBody,
+  (req: Request, res: Response) => userController.login(req, res),
+);
 
-router.get('/role', ValidateLogin.validateToken, (req, res) => userController.getRole(req, res));
+router.get(
+  '/role',
+  ValidateLogin.validateToken,
+  (req: Request, res: Response) => userController.getRole(req, res),
+);
 
 export default router;
