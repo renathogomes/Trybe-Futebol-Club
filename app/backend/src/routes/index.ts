f4de93084320030aee0b0f45aa188806a0302f53
renatho <renatho.20gomes@gmail.com>
2024-03-13 03:15:03 -0300

    index do router

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import teamRouter from './team.routes';
 import userLoginRouter from './login.routes';
 import matchRouter from './match.routes';
+import leaderBoardRouter from './leadboard.routes';
 
 const router = Router();
 
 router.use('/teams', teamRouter);
 router.use('/login', userLoginRouter);
 router.use('/matches', matchRouter);
+router.use('/leaderboard', leaderBoardRouter);
 
 export default router;
